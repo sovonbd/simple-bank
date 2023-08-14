b59535e66143a17eaf15794129820e6ce0ee003a
sovonbd <sovonsdp@gmail.com>
2023-08-14 17:20:11 -0500

    updated

--- js/deposit.js
+++ js/deposit.js
@@ -3,6 +3,10 @@ document.getElementById("btn-deposit").addEventListener("click", function () {
   const newDepositAmount = parseFloat(depositField.value);
   const depositTotalElement = document.getElementById("deposit-total");
   const previousDepositTotal = parseFloat(depositTotalElement.innerText);
+  depositField.value = "";
+  if (isNaN(newDepositAmount)) {
+    return alert("please input a number");
+  }
   const currentDepositTotal = previousDepositTotal + newDepositAmount;
   depositTotalElement.innerText = currentDepositTotal.toFixed(2);
 
@@ -11,5 +15,4 @@ document.getElementById("btn-deposit").addEventListener("click", function () {
   const previousBalanceTotal = parseFloat(balanceTotalElement.innerText);
   const currentBalanceTotal = previousBalanceTotal + newDepositAmount;
   balanceTotalElement.innerText = currentBalanceTotal.toFixed(2);
-  depositField.value = "";
 });
--- js/withdraw.js
+++ js/withdraw.js
@@ -1,26 +1,31 @@
 document.getElementById("btn-withdraw").addEventListener("click", function () {
-  //   const withdrawField = document.getElementById("withdraw-field");
-  //   const withdrawFieldValue = parseFloat(withdrawField.value);
-  //   const withdrawTotal = document.getElementById("withdraw-total");
-  //   const previousWithdrawTotal = parseFloat(withdrawTotal.innerText);
-  //   const currentWithdrawTotal = previousWithdrawTotal + withdrawFieldValue;
-  //   withdrawTotal.innerText = currentWithdrawTotal.toFixed(2);
+  // call the input and convert them to number
+  const withdrawField = parseFloat(document.getElementById("withdraw-field").value);
+  const totalWithdraw = parseFloat(document.getElementById("withdraw-total").innerText);
 
-  //   //   update the balance
-  //   const totalBalance = document.getElementById("balance-total");
-  //   const previousBalanceTotal = parseFloat(totalBalance.innerText);
-  //   const currentBalance = previousBalanceTotal - withdrawFieldValue;
-  //   totalBalance.innerText = currentBalance.toFixed(2);
+  // get the total withdraw
+  const previousWithdraw = totalWithdraw + withdrawField;
 
-  const withdrawField = document.getElementById("withdraw-field");
-  const totalWithdraw = document.getElementById("withdraw-total");
-  const previousWithdraw = parseFloat(totalWithdraw.innerText) + parseFloat(withdrawField.value);
-  totalWithdraw.innerText = previousWithdraw.toFixed(2);
+  // clear the input field
+  document.getElementById("withdraw-field").value = "";
 
-  //   update the balance
-  const totalBalance = document.getElementById("balance-total");
-  const remainingBalance = parseFloat(totalBalance.innerText) - parseFloat(withdrawField.value);
-  totalBalance.innerText = remainingBalance.toFixed(2);
+  // check whether the input is a valid
+  if (isNaN(withdrawField)) {
+    return alert("please input a number");
+  }
 
-  withdrawField.value = "";
+  //   call the text in the balance section
+  const totalBalance = parseFloat(document.getElementById("balance-total").innerText);
+
+  // check whether the withdraw amount is correct
+  if (withdrawField > totalBalance) {
+    return alert("You don't have enough money to withdraw");
+  }
+
+  // set the total withdraw in the withdraw section
+  document.getElementById("withdraw-total").innerText = previousWithdraw.toFixed(2);
+
+  // set the total balance in the balance section
+  const remainingBalance = totalBalance - withdrawField;
+  document.getElementById("balance-total").innerText = remainingBalance.toFixed(2);
 });
